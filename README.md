f9cc9b14fe7865ea747b857d8505e7498408cec2
Najeem Kanishka <contact@najeemkanishka.com>
2022-05-13 01:59:11 -0400

    Updated README and Added Changes to compare.sh

---
+++ README.md
@@ -0,0 +1,16 @@
+# Git-Tools
+A series of tools to help increase your GitHub productivity.
+
+## Tools List
+### compare.sh
+* Example Usage: `./compare.sh branch_to_merge_to`
+* Will compare branch passed in argument against current branch
+* Args: 
+    * `comparison_branch`: Branch to Compare Against
+    * `mode` (optional): Reverse Comparison
+        * `normal` (default): comparison_branch <- current_branch
+        * `reverse`: current_branch <- comparison_branch
+        * `custom`: comparison_branch <- second_branch
+    * `second_branch` (only when arg `mode=custom`):
+        * Branch to Compare Against
+        * comparison_branch <- second_branch
--- compare.sh
+++ compare.sh
@@ -8,11 +8,11 @@ branches=()
 eval "$(git for-each-ref --shell --format='branches+=(%(refname))' refs/heads/ | sed 's/refs\/heads\///')"
 comparison_branch=$1
 if ! containsElement $comparison_branch "${branches[@]}"; then
-    echo "Invalid branch '${comparison_branch}'. Run 'git fetch' to fetch latest branches."
+    echo "Error: Invalid branch '${comparison_branch}'. Run 'git fetch' to fetch latest branches."
     exit 1
 fi
 
-## arg 2 validation: comparison mode selected, and if custom, branch present
+## arg 2 validation: comparison mode selected, and if custom, arg 3 validation branch present
 # n, normal, or blank : comparison_branch <- current repo branch
 # r, reverse,         : current repo branch <- comparison_branch
 # c, custom,          : comparison_branch <- second_comparison_branch
@@ -26,11 +26,19 @@ elif [[ $comparison_mode = "c" ]] || [[ $comparison_mode = "custom" ]]; then
     if containsElement $base_branch "${branches[@]}"; then
         generate_url="${repo}/compare/${comparison_branch}...${base_branch}"
     else
-        echo "Invalid branch '${base_branch}'. Run 'git fetch' to fetch latest branches."
+        echo "Error: Invalid branch '${base_branch}'. Run 'git fetch' to fetch latest branches."
         exit 1
     fi
 else
-    echo "Invalid argument '${comparison_mode}'. Either add argument 'normal', 'reverse', or leave empty."
+    echo "Error: Invalid argument '${comparison_mode}'. Either add argument 'normal', 'reverse', or leave empty."
     exit 1
 fi
-echo $generate_url
+
+
+## ensure that branch not compared against self
+if [[ $comparison_branch = $base_branch ]]; then
+    echo "Error: Attempting to compare branch '${comparison_branch}' against itself."
+    exit 1
+else
+    echo $generate_url
+fi
